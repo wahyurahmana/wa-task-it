59c2724308076cf07b1134ecad8008a2dc01c031
wahyurahmana <tn.wahyurahmana@gmail.com>
2023-12-15 22:05:06 +0800

    /all

--- app.js
+++ app.js
@@ -20,12 +20,12 @@ client.on('message', async (message) =>{
       const sql = 'select * from task where tim ilike $1 and created = $2';
       const values = [message.body.split(' ')[1], message.body.split(' ')[2]]
       const result = await pool.query(sql, values)
-      for (let i = 0; i < result.rows.length; i++) {
-        send.push(`- ${result.rows[i].deskripsi}\n`)
-      }
-      if(send.length === 0){
+      if(!result.rows.length){
         message.reply('Sepertinya Tidak Ada Data Yang Ditemukan Deh Kak')
       }else{
+        for (let i = 0; i < result.rows.length; i++) {
+          send.push(`- ${result.rows[i].deskripsi}\n`)
+        }
         message.reply(send.join(''))
       }
     }else if(message.body.split(' ')[0].toLowerCase() === '/now'){
@@ -33,19 +33,31 @@ client.on('message', async (message) =>{
       const sql = 'select * from task where tim ilike $1 and created = $2';
       const values = [message.body.split(' ')[1], taskNow]
       const result = await pool.query(sql, values)
-      for (let i = 0; i < result.rows.length; i++) {
-        send.push(`- ${result.rows[i].deskripsi}\n`)
-      }
-      if(send.length === 0){
+      if(!result.rows.length){
         message.reply('Sepertinya Tidak Ada Data Yang Ditemukan Deh Kak')
       }else{
+        for (let i = 0; i < result.rows.length; i++) {
+          send.push(`- ${result.rows[i].deskripsi}\n`)
+        }
+        message.reply(send.join(''))
+      }
+    }else if(message.body.split(' ')[0].toLowerCase() === '/all'){
+      const sql = 'select * from task where created = $1';
+      const values = [message.body.split(' ')[1]]
+      const result = await pool.query(sql, values)
+      if(!result.rows.length){
+        message.reply('Sepertinya Tidak Ada Data Yang Ditemukan Deh Kak')
+      }else{
+        for (let i = 0; i < result.rows.length; i++) {
+          send.push(`*${result.rows[i].tim}* - ${result.rows[i].deskripsi}\n`)
+        }
         message.reply(send.join(''))
       }
     }else{
-      message.reply('Halo Kak, Ada Yang Bisa Nelin Bantu?\n\nBerikut Perintah Yang Nelin Mengerti:\n\n/task <nama tim> <tahun-bulan-tanggal>\ncontohnya /task cctv 2023-12-31\n\n/now <nama_tim>\ncontohnya /now cctv')
+      message.reply('Halo Kak, Ada Yang Bisa Nelin Bantu?\n\nBerikut Perintah Yang Nelin Mengerti:\n\n/task <nama tim> <tahun-bulan-tanggal>\ncontohnya: /task cctv 2023-12-31\n\n/now <nama_tim>\ncontohnya: /now cctv\n\n/all <tahun-bulan-tanggal>\ncontohnya: /all 2023-12-31')
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
     message.reply('Duh Nelin Error, Segera Lapor ke wa.me/+6282236464656 ya kak')
   }
 })
