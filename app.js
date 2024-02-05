f81a56cf7ce234396955b49fce299b78597eb97b
wahyurahmana <tn.wahyurahmana@gmail.com>
2024-02-05 12:58:48 +0800

    /add

--- app.js
+++ app.js
@@ -110,15 +110,18 @@ client.on('message', async (message) =>{
       }
       message.reply(msgBody)
     }else if(message.body.split(' ')[0].toLowerCase() === '/add'){
+      //proses membuat created today
       const taskNow =  moment.tz('Asia/Makassar').format().split('T')[0]
+      //proses membuat temporary deksripsi
+      const temp = message.body.split(' ');
+      temp.splice(0,1);
       const sql = 'insert into task(tim, deskripsi, created) values($1, $2, $3) returning task_id;'
-      const values = [message.body.split(' ')[1], message.body.split(' ')[2], taskNow]
+      const values = [message.body.split(' ')[1], temp.join(' '), taskNow]
       const result = await pool.query(sql, values)
       if(!result.rows.length){
         message.reply('Sepertinya Tidak Ada Data Yang Ditambahkan Deh Kak')
       }else{
-        console.log(result.rows);
-        message.reply(`Berhasil Ditambahkan Dengan ID ${result.rows[0]}`)
+        message.reply(`Berhasil Ditambahkan Dengan ID ${result.rows[0].task_id}`)
       }
     }else{
       message.reply('Halo Kak, Ada Yang Bisa Nelin Bantu?\n\nBerikut Perintah Yang Nelin Mengerti:\n\n/task <nama tim> <tahun-bulan-tanggal>\ncontohnya: /task cctv 2023-12-31\n\n/now <nama_tim>\ncontohnya: /now cctv\n\n/all <tahun-bulan-tanggal>\ncontohnya: /all 2023-12-31\n\n/8vital\n\n/done <nomor_task>\ncontohnya: /done 99\n\n/job <nomor_task>\ncontohnya: /job 99\n\n/gempa\n\n/add <nama_tim> <deskripsi>\ncontohnya: /add cctv pm kamera')
